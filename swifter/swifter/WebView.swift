51d9e41c2aa3ecdeef5a4488eafc3e3d8d9d8ec9
Snow Yáng <snow.yang@fluv.com>
2024-08-27 19:37:27 +0800

    Add WebView

---
+++ swifter/swifter/WebView.swift
@@ -0,0 +1,26 @@
+//
+//  WebView.swift
+//  swifter
+//
+//  Created by S on 2024/8/27.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    init(url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context _: Context) -> WKWebView {
+        let webView = WKWebView()
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+
+    func updateUIView(_: WKWebView, context _: Context) {}
+}
